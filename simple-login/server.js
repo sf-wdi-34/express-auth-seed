0ab8e0de95812bd409818b9f93a94be4e30bc3df
Brianna Veenstra <b.g.veenstra@gmail.com>
2017-01-10 12:29:12 -0800

    modifying for angular - server

--- simple-login/server.js
+++ simple-login/server.js
@@ -15,72 +15,96 @@ var User = require('./models/user');
 
 // middleware
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
+// add this so that we can accept request payloads
+app.use(bodyParser.json());
+
 app.use(session({
   saveUninitialized: true,
   resave: true,
   secret: 'SuperSecretCookie',
-  cookie: { maxAge: 30 * 60 * 1000 }
+  cookie: { maxAge: 30 * 60 * 1000 } // 30 minutes in microseconds
 }));
 
-// splash page to show all users
-app.get('/', function (req, res) {
+// HTML routes
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/templates/:name', function templates(req, res) {
+  var name = req.params.name;
+  res.sendFile(__dirname + '/views/templates/' + name + '.html');
+});
+
+// API routes
+
+// get all users
+app.get('/api/users', function (req, res) {
   User.find({}, function(err, allUsers){
     if(allUsers){
-      res.render('index.ejs', { users: allUsers });
+      res.json(allUsers);
     } else {
       res.status(500).send('server error');
     }
   });
 });
 
-// show the signup form
-app.get('/signup', function (req, res) {
-  res.render('signup');
-});
-
+// "sign up"
 // create a user
 app.post('/users', function (req, res) {
-  console.log(req.body)
+  console.log(req.body);
   User.createSecure(req.body.email, req.body.password, function (err, newUser) {
-    req.session.userId = newUser._id;
-    res.redirect('/profile');
+    if (newUser){
+      req.session.userId = newUser._id; // log the user in immediately
+      res.json(newUser);
+    } else {
+      console.log('user creation error:', err);
+      res.status(500).send(err);
+    }
   });
 });
 
-// show the login form
-app.get('/login', function (req, res) {
-  res.render('login');
-});
-
+// "log in"
 // authenticate the user and set the session
 app.post('/sessions', function (req, res) {
   // call authenticate function to check if password user entered is correct
   User.authenticate(req.body.email, req.body.password, function (err, loggedInUser) {
     if (err){
       console.log('authentication error: ', err);
-      res.status(500).send();
+      res.status(500).send(err);
     } else {
       console.log('setting sesstion user id ', loggedInUser._id);
       req.session.userId = loggedInUser._id;
-      res.redirect('/profile');
+      res.json(loggedInUser);
     }
   });
 });
 
-// show user profile page
-app.get('/profile', function (req, res) {
+// get current user information
+app.get('/current-user', function (req, res) {
   console.log('session user id: ', req.session.userId);
   // find the user currently logged in
   User.findOne({_id: req.session.userId}, function (err, currentUser) {
     if (err){
       console.log('database error: ', err);
-      res.redirect('/login');
+      res.status(500).send(err);
     } else {
-      // render profile template with user's data
-      console.log('loading profile of logged in user');
-      res.render('user-show.ejs', {user: currentUser});
+      console.log('found current user');
+      res.json(currentUser);
+    }
+  });
+});
+
+// get one user's information
+app.get('/users/:id', function (req, res) {
+  User.findOne({_id: req.params.id}, function (err, foundUser) {
+    if (err){
+      console.log('database error: ', err);
+      res.status(500).send(err);
+    } else {
+      console.log('found user');
+      res.json(foundUser);
     }
   });
 });
@@ -89,7 +113,8 @@ app.get('/logout', function (req, res) {
   // remove the session user id
   req.session.userId = null;
   // redirect to login (for now)
-  res.redirect('/login');
+  // res.redirect('/login');
+  res.status(200).send('login successful');
 });
 
 // listen on port 3000
